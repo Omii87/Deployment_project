052a6102b28f7c4b2c8a2b1f6da5c9e3a3b5c259
unknown <omkarpethe38@gmail.com>
2023-07-07 01:39:40 +0530

    Adding files to git

---
+++ README.md
@@ -0,0 +1,16 @@
+# medical_insurence
+
+## Problem Statement.
+Predict the insurance value 
+
+# The features are : -
+ 
+ * Age
+ * BMI
+ * Gender
+ * Smoker
+ * Region
+ * Children 
+
+ *This is the data 
+ 
---
+++ interface.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from utils import get_predicted_price
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    
+    return jsonify({"Result":"Successful"})
+
+@app.route('/medical_insurence/predict_charges')
+def predict_charges():
+    data = request.form
+    print("Data :",data)
+    age      = data['age']
+    gender   = data['gender']
+    bmi      = data['bmi']
+    children = data['children']
+    smoker   = data['smoker']
+    region   = data['region']
+
+    pred_price = get_predicted_price(age,gender,bmi,children,smoker,region)
+    
+    return jsonify({"Result":f"Predicted Medical Charges == {pred_price}"})
+ 
+if __name__ == "__main__":
+    app.run()
